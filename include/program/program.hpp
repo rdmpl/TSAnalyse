af087d133d3473039a612d43007f39253659864e
zhgqhf <zhgqhf@qq.com>
2020-12-10 17:40:20 +0800

    added some implememt

--- include/program/program.hpp
+++ include/program/program.hpp
@@ -12,18 +12,43 @@ enum SORT_TYPE {
   SORT_BY_LCN,
   SORT_BY_SID,
 };
+enum PSI_FLAG {
+  PSI_NIT = 0x01,
+  PSI_PAT = 0x02,
+  PSI_PMT = 0x04,
+  PSI_BAT = 0x08,
+  PSI_CAT = 0x10,
+  PSI_SDT_A = 0x20,
+  PSI_SDT_O = 0x40,
+};
 struct prog_node_t {
   uint16_t service_id;
-  uint16_t lcn;
+  uint16_t lcn_value;
+  uint16_t network_id;
   uint16_t ts_id;
-  uint16_t ts_pos;
+  uint16_t tp_idx;
   uint16_t pmt_pid;
-  uint32_t attr;
+  uint16_t video_pid;
+  uint16_t audio_pid;
+  uint16_t pcr_pid;
+  uint16_t ecm_pid;
+  uint32_t service_price;
+  uint32_t psi_flag;  // 标记当前节目存在于哪些表中
   uint32_t bat_attr[MAX_BOUQUET_COUNT];
+  uint32_t reserved;
+  uint8_t parent_lock_flag;
+  uint8_t scramble_flag;
+  uint8_t scramble_type;
+  uint8_t pat_version;
+  uint8_t pmt_version;
+  uint8_t nit_version;
+  uint8_t sdt_version;
+  uint8_t bat_version;
+  uint8_t cat_version;
+  uint8_t service_type;  // radio or video
   int8_t service_name[MAX_SERVICE_NAME_LENGTH];
-  int8_t ca_flag;
-  bool b_use;
-  bool sort_with(prog_node_t *other, SORT_TYPE t, uint8_t as) {
+  bool b_use;  // to flag whether this prog is in use
+  bool compare_pos(prog_node_t *other, SORT_TYPE t, uint8_t as) {
     uint16_t v1 = t == SORT_BY_LCN ? lcn : service_id;
     uint16_t v2 = t == SORT_BY_LCN ? t->lcn : t->service_id;
     return v1 < v2 && as;
@@ -41,10 +66,13 @@ struct bat_info_t {
   int8_t bouquet_name[MAX_SERVICE_NAME_LENGTH];
 };
 struct data_base_t {
-  uint16_t prog_count;
-  prog_node_t prog[MAX_PROG_COUNT];
-  bat_info_t bouquet[32 * MAX_PROG_COUNT];
-  prog_tp_t prog_tp[MAX_TP_COUNT];
+  uint16_t prog_count;  // total count of prog
+  uint16_t view_count;  // count of prog in view
+  uint16_t tp_count;
+  uint16_t bouquet_count;
+  prog_node_t prog_list[MAX_PROG_COUNT];
+  bat_info_t bouquet_list[32 * MAX_PROG_COUNT];
+  prog_tp_t tp_list[MAX_TP_COUNT];
   uint32_t current_attr;
   uint32_t current_bat_attr;
   uint16_t cur_view_list[MAX_PROG_COUNT];
---
+++ source/program/program.cpp
@@ -0,0 +1,30 @@
+#include "program/program.hpp"
+error_type_t data_base_t::add_prog(prog_node_t *prog) {
+  int i = 0;
+  for (i = 0; i < this->prog_count; i++) {
+    if (this->prog_list[i].b_use == false) {
+      // 替换被标记为无效的节目
+      break;
+    }
+  }
+  prog_node_t *prog_in_base = &(this->prog_list[i]);
+  if (i == this->prog_count) {
+    this->prog_count++;
+  }
+  memset(prog_in_base, 0, sizeof(prog_in_base[0]));
+  memcpy(prog_in_base, prog, sizeof(prog_in_base[0]));
+  // TODO: link the bouquet data;
+  // TODO: link the tp data;
+  for (i = 0; i < this->tp_count; i++) {
+    if (this->tp_list[i].ts_id == prog->ts_id) {
+      break;
+    }
+  }
+  prog_in_base->tp_idx = i;
+  if (i == this->tp_count) {
+    memset(&this->tp_list[i], 0, sizeof(this->tp_list[i]));
+    this->tp_list[i].ts_id = prog->ts_id;
+    this->tp_list[i].network_id = prog->network_id;
+    this->tp_count++;
+  }
+}
