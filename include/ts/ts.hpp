93c8521bbf48c30cbd4f083c23d2bf4bd832fc50
zhgqhf <zhgqhf@qq.com>
2020-12-11 17:54:35 +0800

    added ts implements

--- include/ts/ts.hpp
+++ include/ts/ts.hpp
@@ -1,12 +1,17 @@
 #ifndef TS_HPP__
 #define TS_HPP__
 #include <stdint.h>
+#include <stdio.h>
+#define LOG_INFO(fmt, args...) printf("%s(%d)" fmt, __FUNCTION__, __LINE__, ##args)
 struct TS {
-  uint8_t version;
+  uint32_t filer_id;
+  uint8_t table_id;
+  uint16_t section_length;
+  uint8_t version_number;
   uint8_t current_section;
   uint8_t last_section;
   uint32_t crc32;
-  virtual parse(uint8_t *data, uint16_t len, void *priv);
+  virtual bool parse(uint8_t *data, uint16_t len, void *priv);
   virtual ~TS(){};
 };
 #endif  // end of TS_HPP__
