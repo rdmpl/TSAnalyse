5e4dff65b7d96fb182912a5989416d3c63f03d80
zhgqhf <zhou@DESKTOP-BEMH1UQ.localdomain>
2020-12-27 18:34:33 +0800

    rewrite the bat parse module

--- include/ts/bat.hpp
+++ include/ts/bat.hpp
@@ -1,14 +1,62 @@
 #ifndef BAT_HPP__
 #define BAT_HPP__
 #include "ts.hpp"
-/// 允许以下类被继承，从而使其功能得到扩展
 namespace ts {
+#define MAX_BOUQUET_NAME_LEN 32
+// This is a struct to handle the bouquet header info
+struct bouquet_header_t {
+  uint16_t bouquet_id;
+  uint16_t start_up_sid;
+  uint8_t bouquet_name[MAX_BOUQUET_NAME_LEN];
+  virtual bool bouquet_header_parse(uint8_t *data, uint16_t len);
+  virtual ~bouquet_header_t(){};
+};
+// An abstract class to store the data bouquet data.
+// it can be different type according to the tag
+struct bouquet_element_t {
+  uint16_t tag;
+  virtual bool bouquet_element_parse(uint8_t *data, uint16_t len) = 0;
+  virtual bool self_speak(void) = 0;
+  virtual ~bouquet_element_t(){};
+};
+// the tag of service list
+struct bouquet_service_list : public bouquet_element_t {
+  std::vector<std::pair<uint16_t, uint8_t>> service;  // serviceid, service_type
+  virtual bool bouquet_element_parse(uint8_t *data, uint16_t len);
+  virtual bool self_speak(void);
+};
+// the tag of lcn, just for ucn special use;
+struct bouquet_lcn_list : public bouquet_element_t {
+  std::vector<std::pair<uint16_t, uint16_t>> service;  // serviceid, lcn
+  virtual bool bouquet_element_parse(uint8_t *data, uint16_t len);
+  virtual bool self_speak(void);
+};
+struct bouquet_factory_t {
+  virtual bouquet_element_t *create_bouquet_element(uint8_t tag);
+  virtual bouquet_header_t *create_bouquet_header(uint16_t id);
+  virtual ~bouquet_factory_t(){};
+};
+struct bouquet_ts_t {
+  uint16_t ts_id;
+  uint16_t network_id;
+  std::vector<bouquet_element_t *> element;
+  virtual bool bouquet_ts_parse(uint8_t *data, uint16_t len,
+                                bouquet_factory_t *fac);
+  virtual bool self_speak(void);
+};
+// one header with a lot of transport ts
+struct bouquet_info_t {
+  bouquet_header_t *header;
+  std::vector<bouquet_ts_t> ts;
+  bool self_speak(void);
+};
+// bat is a common struct for bouqueit association table
 struct bat : public abstract_ts {
-  std::vector<bat_info_t> bat_info;
-  bat(uint16_t pid) : abstract_ts(pid), bat_info() {}
-  virtual bool dump();
-  bool loop1_parse(uint8_t *data, uint16_t len, bat_info_t &bat_info);
-  bool loop2_parse(uint8_t *data, uint16_t len, bat_info_t &bat_info);
+  std::vector<bouquet_info_t> bouquet;
+  bouquet_factory_t *fac;
+  bat(uint16_t pid)
+      : abstract_ts(pid), fac(new bouquet_factory_t()), bouquet(0) {}
+  bool self_speak(void);
   virtual bool parse(uint8_t *data, uint16_t len, void *priv);
   virtual bool update_callback(void);  // 当有区块更新时回调
   virtual bool finish_callback(void);  // 所有区块更新完回调
--- source/ts/bat.cpp
+++ source/ts/bat.cpp
@@ -8,50 +8,76 @@ uint16_t convert_to_unicode(uint8_t *src, uint16_t len, uint8_t *dest,
   }
   return i;
 }
-bool bat::loop1_parse(uint8_t *data, uint16_t len, bat_info_t &bat) {
-  uint16_t loop_len = 0;
-  while (loop_len < len) {
-    uint8_t desc_tag = data[loop_len];
-    uint8_t desc_len = data[loop_len + 1];
-    switch (desc_tag) {
-      case 0x47:  // bat name
-        bat.bouquet_name_len =
-            convert_to_unicode(data + loop_len + 2, desc_len, bat.bouquet_name,
-                               lengthof(bat.bouquet_name));
-        break;
-      case 0x4a:  // if bid = 0xc0 is the startup program of ucn
-        if (bat.bouquet_id == 0xc0) {
-          uint16_t sid = (data[loop_len + 2] << 8) | data[loop_len + 3];
-          LOG_INFO("start_up service id = {}", sid);
-        }
-        break;
-      default:
-        break;
-    }
-    loop_len += (2 + desc_len);
+
+bool bouquet_service_list::bouquet_element_parse(uint8_t *data, uint16_t len) {
+  uint16_t i = 0;
+  while (i < len) {
+    uint16_t sid = (data[i] << 8) | data[i + 1];
+    uint8_t type = data[i + 2];
+    i += 3;
+    this->service.push_back(std::make_pair(sid, type));
   }
   return true;
 }
-bool bat::loop2_parse(uint8_t *data, uint16_t len, bat_info_t &bat) {
-  uint16_t loop_len = 0;
-  while (loop_len < len) {
-    uint8_t desc_tag = data[loop_len];
-    uint8_t desc_len = data[loop_len + 1];
-    // LOG_INFO("tag = %x", desc_tag);
-    switch (desc_tag) {
-      case 0x41:
-        for (uint8_t p = loop_len + 2; p < loop_len + desc_len + 2; p += 3) {
-          uint16_t service_id = data[p] << 8 | data[p + 1];
-          bat.service_info.push_back({service_id, 1});
-        }
-        break;
-      case 0x83:
-        // UCN's lcn parse
-        break;
-      default:
-        break;
+bool bouquet_lcn_list::bouquet_element_parse(uint8_t *data, uint16_t len) {
+  uint16_t i = 0;
+  while (i < len) {
+    uint16_t sid = (data[i] << 8) | data[i + 1];
+    uint16_t lcn = ((data[i + 2] & 0x1f) << 8) | data[i + 3];
+    this->service.push_back(std::make_pair(sid, lcn));
+    i += 4;
+  }
+  return true;
+}
+
+bool bouquet_header_t::bouquet_header_parse(uint8_t *data, uint16_t size) {
+  uint16_t i = 0;
+  while (i < size) {
+    uint8_t tag = data[i];
+    uint8_t len = data[i + 1];
+    if (tag == 0x47) {  //
+      convert_to_unicode(data + i + 2, len, this->bouquet_name,
+                         lengthof(this->bouquet_name));
+    } else if (tag == 0x4a) {
+      uint16_t start_up_sid = (data[i + 2] << 8) | data[i + 3];
     }
-    loop_len += (2 + desc_len);
+    i += (2 + len);
+  }
+  return true;
+}
+bouquet_element_t *bouquet_factory_t::create_bouquet_element(uint8_t tag) {
+  bouquet_element_t *ele = nullptr;
+  if (tag == 0x41) {
+    ele = new bouquet_service_list();
+  } else if (tag == 0x83) {
+    ele = new bouquet_lcn_list();
+  }
+  if (ele) {
+    ele->tag = tag;
+  }
+  return ele;
+}
+bouquet_header_t *bouquet_factory_t::create_bouquet_header(uint16_t id) {
+  bouquet_header_t *header = nullptr;
+  header = new bouquet_header_t();
+  header->bouquet_id = id;
+  return header;
+}
+bool bouquet_ts_t::bouquet_ts_parse(uint8_t *data, uint16_t size,
+                                    bouquet_factory_t *fac) {
+  if (data == nullptr || fac == nullptr) {
+    return false;
+  }
+  uint16_t i = 0;
+  while (i < size) {
+    uint8_t tag = data[i];
+    uint8_t len = data[i + 1];
+    bouquet_element_t *ele = fac->create_bouquet_element(tag);
+    if (ele != nullptr) {
+      this->element.push_back(ele);
+      ele->bouquet_element_parse(data + i + 2, len);
+    }
+    i += (2 + len);
   }
   return true;
 }
@@ -63,43 +89,73 @@ bool bat::parse(uint8_t *data, uint16_t len, void *priv) {
   }
   uint16_t bouquet_id = (data[3] << 8) | data[4];
   uint16_t i = 0;
-  while (i < this->bat_info.size() &&
-         this->bat_info[i].bouquet_id != bouquet_id) {
+  while (i < this->bouquet.size() &&
+         this->bouquet[i].header->bouquet_id != bouquet_id) {
     ++i;
   }
-  if (i >= this->bat_info.size()) {
-    this->bat_info.push_back(bat_info_t());
+  if (i >= this->bouquet.size()) {
+    this->bouquet.push_back(bouquet_info_t());
   }
-  bat_info_t &bat_info = this->bat_info[i];
-  bat_info.bouquet_id = bouquet_id;
-  uint16_t loop1_len = getu16len(data + 8, false);  //
-  this->loop1_parse(data + 10, loop1_len, bat_info);
-  uint16_t loop2_begin = 10 + loop1_len;
+  bouquet_info_t &bat_info = this->bouquet[i];
+  bat_info.header = fac->create_bouquet_header(bouquet_id);
+  uint16_t header_len = getu16len(data + 8, false);  //
+  bat_info.header->bouquet_header_parse(data + 10, header_len);
+  uint16_t loop2_begin = 10 + header_len;
   // TODO: notify other thread to parse loop1 data
   uint16_t loop2_end = loop2_begin + 2 + getu16len(data + loop2_begin, false);
   loop2_begin += 2;
   while (loop2_begin < loop2_end) {
-    bat_info.ts_id = (data[loop2_begin] << 8) | data[loop2_begin + 1];
-    bat_info.network_id = (data[loop2_begin + 2] << 8) | data[loop2_begin + 3];
+    bouquet_ts_t ts;
+    ts.ts_id = (data[loop2_begin] << 8) | data[loop2_begin + 1];
+    ts.network_id = (data[loop2_begin + 2] << 8) | data[loop2_begin + 3];
     uint16_t data_len = getu16len(data + loop2_begin + 4, false);
-    this->loop2_parse(data + loop2_begin + 6, data_len, bat_info);
+    ts.bouquet_ts_parse(data + loop2_begin + 6, data_len, fac);
     loop2_begin += (6 + data_len);
+    bat_info.ts.emplace_back(ts);
   }
   return true;
 }
-bool bat::dump(void) {
-  for (uint16_t i = 0; i < this->bat_info.size(); ++i) {
-    printf("\n\n%d: bat_id = 0x%x\n", i, this->bat_info[i].bouquet_id);
-    for (uint16_t j = 0; j < this->bat_info[i].service_info.size(); ++j) {
-      printf("0x%x  ", this->bat_info[i].service_info[j].first);
-    }
+bool bouquet_lcn_list::self_speak(void) {
+  for (int i = 0; i < this->service.size(); ++i) {
+    fmt::print("sid(lcn):{:04x}({:d})  ", this->service[i].first,
+               this->service[i].second);
+  }
+  return true;
+}
+bool bouquet_service_list::self_speak(void) {
+  for (int i = 0; i < this->service.size(); ++i) {
+    fmt::print("0x{:04x}({:d})  ", this->service[i].first,
+               this->service[i].second);
+  }
+  return true;
+}
+bool bouquet_ts_t::self_speak(void) {
+  fmt::print("ts_id = 0x{:04x}, network_id = {:04x}\n", this->ts_id,
+             this->network_id);
+  for (int i = 0; i < this->element.size(); ++i) {
+    this->element[i]->self_speak();
+  }
+  fmt::print("\n");
+  return true;
+}
+bool bouquet_info_t::self_speak(void) {
+  fmt::print("\n\nbouquet_id = 0x{:04x}, ts_count = {:d}\n",
+             this->header->bouquet_id, this->ts.size());
+  for (int i = 0; i < this->ts.size(); ++i) {
+    this->ts[i].self_speak();
+  }
+  return true;
+}
+bool bat::self_speak(void) {
+  for (uint16_t i = 0; i < this->bouquet.size(); ++i) {
+    this->bouquet[i].self_speak();
   }
   return true;
 }
 bool bat::update_callback(void) { return true; }  // 当有区块更新时回调
 bool bat::finish_callback(void) {
-  LOG_INFO("all bat finished: bat_num = {}", this->bat_info.size());
-  dump();
+  LOG_INFO("all bat finished: bat_num = {}", this->bouquet.size());
+  this->self_speak();
   return true;
 }  // 所有区块更新完回调
 }  // namespace ts
