51113b859daf9bde3be8858115da781a319dff5c
zhgqhf <zhou@DESKTOP-BEMH1UQ.localdomain>
2020-12-27 20:21:50 +0800

    modified the nit implementation

--- include/ts/nit.hpp
+++ include/ts/nit.hpp
@@ -5,26 +5,59 @@
 #include "ts.hpp"
 #include "vector"
 namespace ts {
+// the header of nit table
+struct nit_header_t {
+  uint16_t network_id;
+};
+// to parse the network information, for example,
+// the ota infomation
+struct nit_network_t {
+  virtual bool nit_network_parse(uint8_t *data, uint16_t size);
+  virtual ~nit_network_t(){};
+};
+// nit ts information, including the freq info
+// and service with it
+struct nit_ts_t {
+  uint16_t ts_id;
+  uint16_t original_network_id;
+  uint16_t network_id;
+  uint32_t freq;
+  uint16_t sym;
+  uint8_t mod;  // modulation
+  uint8_t fec_inner;
+  std::vector<std::pair<uint16_t, uint8_t>> service_list;
+  virtual bool self_speak(void);
+  virtual bool nit_ts_parse(uint8_t *data, uint16_t size);
+  virtual ~nit_ts_t();
+};
+// the nit information
+struct nit_info_t {
+  nit_header_t *header;
+  nit_network_t *net;
+  std::vector<nit_ts_t *> ts;
+  virtual bool self_speak(void);
+  virtual ~nit_info_t();
+};
+// the factory of nit information, you can inherit this
+// struct to define your own parse function
+struct nit_factory_t {
+  virtual nit_header_t *create_nit_header(uint16_t original_network_id);
+  virtual nit_ts_t *create_nit_ts(uint16_t ts_id);
+  virtual nit_network_t *create_nit_network(void);
+  virtual ~nit_factory_t(){};
+};
 struct nit : public abstract_ts {
-  std::vector<prog_tp_t> tp_info;
+  std::vector<nit_info_t> nit_info;
+  nit_factory_t *nit_fac;  // used to generate the nit info
   virtual bool parse(uint8_t *data, uint16_t len, void *priv);
   virtual bool update_callback(void);  // 当有区块更新时回调
   virtual bool finish_callback(void);  // 所有区块更新完回调
-  virtual bool loop1_parse(uint8_t *data, uint16_t len);
-  bool dump(void) {
-    for (uint16_t i = 0; i < this->tp_info.size(); ++i) {
-      printf("\n%d: network_id = %x\n", i, tp_info[i].network_id);
-      printf("ts_id: 0x%x, frq: %d, sym: %d, mod = %d\n", tp_info[i].ts_id,
-             tp_info[i].freq, tp_info[i].sym, tp_info[i].mod);
-      for (uint16_t j = 0; j < tp_info[i].service_info.size(); ++j) {
-        printf("0x%x ", tp_info[i].service_info[j].first);
-      }
-      printf("\n");
-    }
-    return true;
-  }
-  nit(uint16_t pid) : abstract_ts(pid), tp_info() {}
-  // TODO: 要添加loop1和loop2的解析函数
+  nit(uint16_t pid)
+      : abstract_ts(pid), nit_info(0), nit_fac(new nit_factory_t()){};
+  nit(uint16_t pid, nit_factory_t *fac)
+      : abstract_ts(pid), nit_info(0), nit_fac(fac){};
+  bool self_speak(void);
+  virtual ~nit();
 };
 }  // namespace ts
 #endif  // NIT_HPP__
--- source/ts/nit.cpp
+++ source/ts/nit.cpp
@@ -1,6 +1,43 @@
 #include <cstring>
 #include <ts/nit.hpp>
 namespace ts {
+nit_info_t::~nit_info_t() {
+  if (net) {
+    delete (net);
+  }
+  if (header) {
+    delete (header);
+  }
+  for (int i = 0; i < ts.size(); ++i) {
+    if (ts[i]) {
+      delete (ts[i]);
+      ts[i] = nullptr;
+    }
+  }
+}
+nit::~nit() {
+  if (nit_fac) {
+    delete (nit_fac);
+  }
+}
+nit_ts_t::~nit_ts_t() {}
+bool nit_network_t::nit_network_parse(uint8_t *data, uint16_t size) {
+  return true;
+}
+nit_header_t *nit_factory_t::create_nit_header(uint16_t original_network_id) {
+  nit_header_t *header = new nit_header_t();
+  header->network_id = original_network_id;
+  return header;
+}
+nit_ts_t *nit_factory_t::create_nit_ts(uint16_t ts_id) {
+  nit_ts_t *ts = new nit_ts_t();
+  ts->ts_id = ts_id;
+  return ts;
+}
+nit_network_t *nit_factory_t::create_nit_network(void) {
+  nit_network_t *net = new nit_network_t();
+  return net;
+}
 bool freq_parse(uint8_t *data, uint8_t len, prog_tp_t &tp) {
   /**
    * frequency:32
@@ -17,19 +54,18 @@ bool freq_parse(uint8_t *data, uint8_t len, prog_tp_t &tp) {
   tp.sym = bcd2integer(data + 7, 3);  // 这里还有点问题,可能精度不够，但不管了
   return true;
 }
-bool service_list_parse(uint8_t *data, uint8_t len, prog_tp_t &tp) {
+bool service_list_parse(uint8_t *data, uint8_t len, nit_ts_t *tp) {
   uint8_t i = 0;
   while (i < len) {
     uint16_t service_id = (data[i] << 8) | data[i + 1];
     uint8_t service_type = data[i + 2];
-    tp.service_info.push_back({service_id, service_type});
+    tp->service_list.push_back({service_id, service_type});
     i += 3;
     // LOG_INFO("service i = %d, len = %d", i, len);
   }
   return true;
 }
-bool nit::loop1_parse(uint8_t *data, uint16_t len) { return true; }
-bool loop2_parse(uint8_t *data, uint16_t len, prog_tp_t &tp) {
+bool nit_ts_t::nit_ts_parse(uint8_t *data, uint16_t len) {
   uint16_t i = 0;
   while (i < len) {
     uint8_t tag = data[i];
@@ -37,10 +73,13 @@ bool loop2_parse(uint8_t *data, uint16_t len, prog_tp_t &tp) {
     i += 2;
     switch (tag) {
       case 0x44:  // freq info
-        freq_parse(data + i, tag_len, tp);
+        this->freq = bcd2integer(data + i, 4) / 100;
+        this->mod = data[i + 6];
+        this->sym = bcd2integer(data + i + 7, 3);
+        this->fec_inner = data[i + 10] & 0xf;
         break;
       case 0x41:  // service list
-        service_list_parse(data + i, tag_len, tp);
+        service_list_parse(data + i, tag_len, this);
         break;
       case 0x83:
         break;  // TODO: lcn parse
@@ -58,35 +97,64 @@ bool nit::parse(uint8_t *data, uint16_t len, void *priv) {
     return false;
   }
   uint16_t original_network_id = (data[3] << 8) | data[4];
+  int i = 0;
+  for (i = 0; i < this->nit_info.size(); ++i) {
+    if (this->nit_info[i].header->network_id == original_network_id) {
+      break;
+    }
+  }
+  if (i == this->nit_info.size()) {
+    this->nit_info.push_back(nit_info_t());
+  }
+  nit_info_t &nit = this->nit_info[i];
+  nit.net = nit_fac->create_nit_network();
+  nit.header = nit_fac->create_nit_header(original_network_id);
   uint16_t loop1_len = getu16len(data + 8, false);
-  loop1_parse(data + 10, loop1_len);
+  nit.net->nit_network_parse(data + 10, loop1_len);  // now not care
   uint16_t loop2_begin = 10 + loop1_len;
   // TODO: notify other thread to parse loop1 data
   uint16_t loop2_end = loop2_begin + 2 + getu16len(data + loop2_begin, false);
   loop2_begin += 2;
   while (loop2_begin < loop2_end) {
     uint16_t ts_id = (data[loop2_begin] << 8) | data[loop2_begin + 1];
-    int k = 0;
-    while (k < this->tp_info.size() && this->tp_info[k].ts_id != ts_id) {
-      ++k;
-    }
-    if (k == this->tp_info.size()) {
-      this->tp_info.push_back(prog_tp_t());
-    }
-    prog_tp_t &tp = this->tp_info[k];
-    tp.original_network_id = original_network_id;
-    tp.network_id = (data[loop2_begin + 2] << 8) | data[loop2_begin + 3];
+    nit_ts_t *ts = nit_fac->create_nit_ts(ts_id);
+    nit.ts.push_back(ts);
+    ts->network_id = (data[loop2_begin + 2] << 8) | data[loop2_begin + 3];
     uint16_t data_len = getu16len(data + loop2_begin + 4, false);
-    loop2_parse(data + loop2_begin + 6, data_len, tp);
-    this->tp_info.emplace_back(tp);
+    ts->nit_ts_parse(data + loop2_begin + 6, data_len);
     loop2_begin += (6 + data_len);
   }
   return false;
 }
+bool nit_ts_t::self_speak(void) {
+  fmt::print("ts_id = 0x{:04x}, freq = {:d}, mod = {:d}, service cnt = {}.\n",
+             ts_id, freq, mod, service_list.size());
+  for (int i = 0; i < service_list.size(); ++i) {
+    fmt::print("0x{:4x}({})  ", service_list[i].first, service_list[i].second);
+  }
+  fmt::print("\n\n");
+  return true;
+}
+bool nit_info_t::self_speak(void) {
+  fmt::print("network_id = 0x{:04x}. ts_count = {:d}\n", header->network_id,
+             this->ts.size());
+  for (int i = 0; i < this->ts.size(); ++i) {
+    this->ts[i]->self_speak();
+  }
+  fmt::print("\n");
+  return true;
+}
+bool nit::self_speak(void) {
+  fmt::print("original net count = {}\n", this->nit_info.size());
+  for (int i = 0; i < this->nit_info.size(); ++i) {
+    this->nit_info[i].self_speak();
+  }
+  return true;
+}
 bool nit::update_callback(void) { return true; };  // 当有区块更新时回调
 bool nit::finish_callback(void) {
   LOG_INFO("all nit data finished.\n");
-  dump();
+  self_speak();
   return true;
 };  // 所有区块更新完回调
 }  // namespace ts
